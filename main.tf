2b0299764a7439bfdab7315f72253498f811cfb5
Riccardo Crippa <therickys93@gmail.com>
2026-04-01 19:24:40 +0200

    wip on adding the ip addresses.

--- main.tf
+++ main.tf
@@ -1,159 +1,260 @@
 # Copyright (c) 2024 BB Tech Systems LLC
 
 locals {
-    primary_control_node_ip = proxmox_virtual_environment_vm.talos_control_vm[keys(var.control_nodes)[0]].ipv4_addresses[7][0]
-    control_node_ips = [for vm in keys(var.control_nodes) : proxmox_virtual_environment_vm.talos_control_vm[vm].ipv4_addresses[7][0]]
-    worker_node_ips = [for vm in keys(var.worker_nodes) : proxmox_virtual_environment_vm.talos_worker_vm[vm].ipv4_addresses[7][0]]
-    node_ips = concat(
-        local.control_node_ips,
-        local.worker_node_ips
-    )
+  # Resolve IP for each control node: prefer static IP if provided, else read from VM after boot
+  control_node_ips = [
+    for vm in keys(var.control_nodes) :
+    length(var.control_plane_ip_addresses) > 0
+      ? split("/", lookup(var.control_plane_ip_addresses, vm, ""))[0]
+      : proxmox_virtual_environment_vm.talos_control_vm[vm].ipv4_addresses[7][0]
+  ]
+
+  # Resolve IP for each worker node: prefer static IP if provided, else read from VM after boot
+  worker_node_ips = [
+    for vm in keys(var.worker_nodes) :
+    length(var.worker_ip_addresses) > 0
+      ? split("/", lookup(var.worker_ip_addresses, vm, ""))[0]
+      : proxmox_virtual_environment_vm.talos_worker_vm[vm].ipv4_addresses[7][0]
+  ]
+
+  primary_control_node_ip = local.control_node_ips[0]
+
+  node_ips = concat(
+    local.control_node_ips,
+    local.worker_node_ips
+  )
+
+  # Talos machine config patch for static IP on control plane nodes.
+  # Generated only for nodes that have an entry in control_plane_ip_addresses.
+  control_static_ip_patches = {
+    for name, cidr in var.control_plane_ip_addresses :
+    name => yamlencode({
+      machine = {
+        network = {
+          interfaces = [
+            {
+              deviceSelector = {
+                driver = "virtio_net"
+              }
+              addresses = [cidr]
+              dhcp      = false
+              routes = var.network_gateway != null ? [
+                {
+                  network = "0.0.0.0/0"
+                  gateway = var.network_gateway
+                }
+              ] : []
+            }
+          ]
+        }
+      }
+    })
+  }
+
+  # Talos machine config patch for static IP on worker nodes.
+  # Generated only for nodes that have an entry in worker_ip_addresses.
+  worker_static_ip_patches = {
+    for name, cidr in var.worker_ip_addresses :
+    name => yamlencode({
+      machine = {
+        network = {
+          interfaces = [
+            {
+              deviceSelector = {
+                driver = "virtio_net"
+              }
+              addresses = [cidr]
+              dhcp      = false
+              routes = var.network_gateway != null ? [
+                {
+                  network = "0.0.0.0/0"
+                  gateway = var.network_gateway
+                }
+              ] : []
+            }
+          ]
+        }
+      }
+    })
+  }
 }
 
 resource "proxmox_virtual_environment_download_file" "talos_image" {
-    content_type = "iso"
-    datastore_id = var.proxmox_iso_datastore
-    node_name    = values(var.control_nodes)[0]
-    url          = "https://factory.talos.dev/image/${var.talos_schematic_id}/v${var.talos_version}/metal-${var.talos_arch}.qcow2"
-    file_name    = "${var.talos_cluster_name}-talos_linux-${var.talos_schematic_id}-${var.talos_version}-${var.talos_arch}.img"
+  content_type = "iso"
+  datastore_id = var.proxmox_iso_datastore
+  node_name    = values(var.control_nodes)[0]
+
+  url       = "https://factory.talos.dev/image/${var.talos_schematic_id}/v${var.talos_version}/metal-${var.talos_arch}.qcow2"
+  file_name = "${var.talos_cluster_name}-talos_linux-${var.talos_schematic_id}-${var.talos_version}-${var.talos_arch}.img"
 }
 
 resource "proxmox_virtual_environment_vm" "talos_control_vm" {
-    for_each  = var.control_nodes
-    name      = each.key
-    node_name = each.value
-    pool_id = var.proxmox_control_pool_id
-    agent {
-        enabled = true
-    }
-    cpu {
-        cores = var.proxmox_control_vm_cores
-        type  = var.proxmox_vm_type
-    }
-    memory {
-        dedicated = var.proxmox_control_vm_memory
-        floating  = var.proxmox_control_vm_memory
-    }
-    disk {
-        datastore_id = var.proxmox_image_datastore
-        file_id      = proxmox_virtual_environment_download_file.talos_image.id
-        interface    = "virtio0"
-        iothread     = true
-        discard      = "on"
-        size         = var.proxmox_control_vm_disk_size
-    }
-    network_device {
-        vlan_id     = var.proxmox_network_vlan_id
-        bridge      = var.proxmox_network_bridge
-        mac_address = lookup(var.control_plane_mac_addresses, each.key, null)
-    }
-    operating_system {
-        type = "l26"
-    }
+  for_each = var.control_nodes
+
+  name      = each.key
+  node_name = each.value
+  pool_id   = var.proxmox_control_pool_id
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = var.proxmox_control_vm_cores
+    type  = var.proxmox_vm_type
+  }
+
+  memory {
+    dedicated = var.proxmox_control_vm_memory
+    floating  = var.proxmox_control_vm_memory
+  }
+
+  disk {
+    datastore_id = var.proxmox_image_datastore
+    file_id      = proxmox_virtual_environment_download_file.talos_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = var.proxmox_control_vm_disk_size
+  }
+
+  network_device {
+    vlan_id     = var.proxmox_network_vlan_id
+    bridge      = var.proxmox_network_bridge
+    mac_address = lookup(var.control_plane_mac_addresses, each.key, null)
+  }
+
+  operating_system {
+    type = "l26"
+  }
 }
 
 resource "proxmox_virtual_environment_vm" "talos_worker_vm" {
-    for_each  = var.worker_nodes
-    name      = each.key
-    node_name = each.value
-    pool_id = var.proxmox_worker_pool_id
-    agent {
-        enabled = true
-    }
-    cpu {
-        cores = var.proxmox_worker_vm_cores
-        type  = var.proxmox_vm_type
-    }
-    memory {
-        dedicated = var.proxmox_worker_vm_memory
-        floating  = var.proxmox_worker_vm_memory
-    }
-    disk {
-        datastore_id = var.proxmox_image_datastore
-        file_id      = proxmox_virtual_environment_download_file.talos_image.id
-        interface    = "virtio0"
-        iothread     = true
-        discard      = "on"
-        size         = var.proxmox_worker_vm_disk_size
-    }
-    network_device {
-        vlan_id     = var.proxmox_network_vlan_id
-        bridge      = var.proxmox_network_bridge
-        mac_address = lookup(var.worker_mac_addresses, each.key, null)
-    }
-    dynamic "disk" {
-        for_each = lookup(var.worker_extra_disks, each.key, [])
-        content {
-            datastore_id = disk.value.datastore_id
-            file_format  = disk.value.file_format
-            file_id      = disk.value.file_id
-            interface    = "virtio${disk.key+1}"
-            iothread     = true
-            discard      = "on"
-            size         = disk.value.size
-        }
+  for_each = var.worker_nodes
 
+  name      = each.key
+  node_name = each.value
+  pool_id   = var.proxmox_worker_pool_id
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = var.proxmox_worker_vm_cores
+    type  = var.proxmox_vm_type
+  }
+
+  memory {
+    dedicated = var.proxmox_worker_vm_memory
+    floating  = var.proxmox_worker_vm_memory
+  }
+
+  disk {
+    datastore_id = var.proxmox_image_datastore
+    file_id      = proxmox_virtual_environment_download_file.talos_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = var.proxmox_worker_vm_disk_size
+  }
+
+  network_device {
+    vlan_id     = var.proxmox_network_vlan_id
+    bridge      = var.proxmox_network_bridge
+    mac_address = lookup(var.worker_mac_addresses, each.key, null)
+  }
+
+  dynamic "disk" {
+    for_each = lookup(var.worker_extra_disks, each.key, [])
+    content {
+      datastore_id = disk.value.datastore_id
+      file_format  = disk.value.file_format
+      file_id      = disk.value.file_id
+      interface    = "virtio${disk.key + 1}"
+      iothread     = true
+      discard      = "on"
+      size         = disk.value.size
     }
-    operating_system {
-        type = "l26"
-    }
+  }
+
+  operating_system {
+    type = "l26"
+  }
 }
 
 resource "talos_machine_secrets" "talos_secrets" {}
 
 data "talos_machine_configuration" "control_mc" {
-    cluster_name     = var.talos_cluster_name
-    machine_type     = "controlplane"
-    # TODO - Should we allow the user to override this?
-    # This is a single point of failure but without a proxy or load balancer
-    # it is required to be a single point of failure.
-    cluster_endpoint = "https://${local.primary_control_node_ip}:6443"
-    machine_secrets  = talos_machine_secrets.talos_secrets.machine_secrets
+  cluster_name = var.talos_cluster_name
+  machine_type = "controlplane"
+
+  # TODO - Should we allow the user to override this?
+  # This is a single point of failure but without a proxy or load balancer
+  # it is required to be a single point of failure.
+  cluster_endpoint = "https://${local.primary_control_node_ip}:6443"
+  machine_secrets  = talos_machine_secrets.talos_secrets.machine_secrets
 }
 
 data "talos_machine_configuration" "worker_mc" {
-    cluster_name     = var.talos_cluster_name
-    machine_type     = "worker"
-    # TODO - Should we allow the user to override this?
-    # This is a single point of failure but without a proxy or load balancer
-    # it is required to be a single point of failure.
-    cluster_endpoint = "https://${local.primary_control_node_ip}:6443"
-    machine_secrets  = talos_machine_secrets.talos_secrets.machine_secrets
+  cluster_name = var.talos_cluster_name
+  machine_type = "worker"
+
+  # TODO - Should we allow the user to override this?
+  # This is a single point of failure but without a proxy or load balancer
+  # it is required to be a single point of failure.
+  cluster_endpoint = "https://${local.primary_control_node_ip}:6443"
+  machine_secrets  = talos_machine_secrets.talos_secrets.machine_secrets
 }
 
 data "talos_client_configuration" "talos_client_config" {
-    cluster_name         = var.talos_cluster_name
-    client_configuration = talos_machine_secrets.talos_secrets.client_configuration
-    endpoints            = local.control_node_ips
-    nodes                = local.node_ips
+  cluster_name         = var.talos_cluster_name
+  client_configuration = talos_machine_secrets.talos_secrets.client_configuration
+  endpoints            = local.control_node_ips
+  nodes                = local.node_ips
 }
 
 resource "talos_machine_configuration_apply" "talos_control_mc_apply" {
-    for_each = var.control_nodes
-    client_configuration        = talos_machine_secrets.talos_secrets.client_configuration
-    machine_configuration_input = data.talos_machine_configuration.control_mc.machine_configuration
-    node                        = proxmox_virtual_environment_vm.talos_control_vm[each.key].ipv4_addresses[7][0]
-    config_patches              = var.control_machine_config_patches
+  for_each = var.control_nodes
+
+  client_configuration        = talos_machine_secrets.talos_secrets.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.control_mc.machine_configuration
+  node                        = local.control_node_ips[index(keys(var.control_nodes), each.key)]
+
+  config_patches = concat(
+    var.control_machine_config_patches,
+    contains(keys(local.control_static_ip_patches), each.key)
+      ? [local.control_static_ip_patches[each.key]]
+      : []
+  )
 }
 
 resource "talos_machine_configuration_apply" "talos_worker_mc_apply" {
-    for_each = var.worker_nodes
-    client_configuration        = talos_machine_secrets.talos_secrets.client_configuration
-    machine_configuration_input = data.talos_machine_configuration.worker_mc.machine_configuration
-    node                        = proxmox_virtual_environment_vm.talos_worker_vm[each.key].ipv4_addresses[7][0]
-    config_patches              = var.worker_machine_config_patches
+  for_each = var.worker_nodes
+
+  client_configuration        = talos_machine_secrets.talos_secrets.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.worker_mc.machine_configuration
+  node                        = local.worker_node_ips[index(keys(var.worker_nodes), each.key)]
+
+  config_patches = concat(
+    var.worker_machine_config_patches,
+    contains(keys(local.worker_static_ip_patches), each.key)
+      ? [local.worker_static_ip_patches[each.key]]
+      : []
+  )
 }
 
 # You only need to bootstrap 1 control node, we pick the first one
 resource "talos_machine_bootstrap" "talos_bootstrap" {
-    node                 = local.primary_control_node_ip
-    client_configuration = talos_machine_secrets.talos_secrets.client_configuration
+  node                 = local.primary_control_node_ip
+  client_configuration = talos_machine_secrets.talos_secrets.client_configuration
 }
 
 resource "talos_cluster_kubeconfig" "talos_kubeconfig" {
-    depends_on   = [
-        talos_machine_bootstrap.talos_bootstrap
-    ]
-    client_configuration = talos_machine_secrets.talos_secrets.client_configuration
-    node                 = local.primary_control_node_ip
+  depends_on = [
+    talos_machine_bootstrap.talos_bootstrap
+  ]
+
+  client_configuration = talos_machine_secrets.talos_secrets.client_configuration
+  node                 = local.primary_control_node_ip
 }
--- variables.tf
+++ variables.tf
@@ -1,121 +1,142 @@
 # Copyright (c) 2024 BB Tech Systems LLC
 
 variable "proxmox_iso_datastore" {
-    description = "Datastore to put the qcow2 image"
-    type        = string
-    default     = "local"
+  description = "Datastore to put the qcow2 image"
+  type        = string
+  default     = "local"
 }
 
 variable "proxmox_image_datastore" {
-    description = "Datastore to put the VM hard drive images"
-    type        = string
-    default     = "local-lvm"
+  description = "Datastore to put the VM hard drive images"
+  type        = string
+  default     = "local-lvm"
 }
 
 variable "proxmox_control_vm_cores" {
-    description = "Number of CPU cores for the control VMs"
-    type        = number
-    default     = 4
+  description = "Number of CPU cores for the control VMs"
+  type        = number
+  default     = 4
 }
 
 variable "proxmox_worker_vm_cores" {
-    description = "Number of CPU cores for the worker VMs"
-    type        = number
-    default     = 4
+  description = "Number of CPU cores for the worker VMs"
+  type        = number
+  default     = 4
 }
 
 variable "proxmox_control_vm_memory" {
-    description = "Memory in MB for the control VMs"
-    type        = number
-    default     = 4096
+  description = "Memory in MB for the control VMs"
+  type        = number
+  default     = 4096
 }
 
 variable "proxmox_worker_vm_memory" {
-    description = "Memory in MB for the worker VMs"
-    type        = number
-    default     = 4096
+  description = "Memory in MB for the worker VMs"
+  type        = number
+  default     = 4096
 }
 
 variable "proxmox_vm_type" {
-    description = "Proxmox emulated CPU type, x86-64-v2-AES recommended"
-    type        = string
-    default     = "x86-64-v2-AES"
+  description = "Proxmox emulated CPU type, x86-64-v2-AES recommended"
+  type        = string
+  default     = "x86-64-v2-AES"
 }
 
 variable "proxmox_control_vm_disk_size" {
-    description = "Proxmox control VM disk size in GB"
-    type        = number
-    default     = 32
+  description = "Proxmox control VM disk size in GB"
+  type        = number
+  default     = 32
 }
 
 variable "proxmox_worker_vm_disk_size" {
-    description = "Proxmox worker VM disk size in GB"
-    type        = number
-    default     = 100
+  description = "Proxmox worker VM disk size in GB"
+  type        = number
+  default     = 100
 }
 
 variable "proxmox_network_vlan_id" {
-    description = "Proxmox network VLAN ID"
-    type        = number
-    default     = null
+  description = "Proxmox network VLAN ID"
+  type        = number
+  default     = null
 }
+
 variable "proxmox_network_bridge" {
   description = "Proxmox network Bridge"
-  type = string
-  default = "vmbr0"
+  type        = string
+  default     = "vmbr0"
 }
 
 variable "control_plane_mac_addresses" {
-    description = "Map of control plane node names to MAC addresses for static IP assignment via DHCP"
-    type        = map(string)
-    default     = {}
-    nullable    = false
+  description = "Map of control plane node names to MAC addresses for static IP assignment via DHCP"
+  type        = map(string)
+  default     = {}
+  nullable    = false
 }
 
 variable "worker_mac_addresses" {
-    description = "Map of worker node names to MAC addresses for static IP assignment via DHCP"
-    type        = map(string)
-    default     = {}
-    nullable    = false
+  description = "Map of worker node names to MAC addresses for static IP assignment via DHCP"
+  type        = map(string)
+  default     = {}
+  nullable    = false
+}
+
+variable "control_plane_ip_addresses" {
+  description = "Map of control plane node names to static IP addresses in CIDR notation (e.g. 192.168.1.10/24). When set, a Talos machine config patch is automatically generated to configure the interface statically."
+  type        = map(string)
+  default     = {}
+  nullable    = false
+}
+
+variable "worker_ip_addresses" {
+  description = "Map of worker node names to static IP addresses in CIDR notation (e.g. 192.168.1.20/24). When set, a Talos machine config patch is automatically generated to configure the interface statically."
+  type        = map(string)
+  default     = {}
+  nullable    = false
+}
+
+variable "network_gateway" {
+  description = "Default gateway used when configuring static IP addresses via control_plane_ip_addresses or worker_ip_addresses"
+  type        = string
+  default     = null
 }
 
 variable "talos_cluster_name" {
-    description = "Name of the Talos cluster"
-    type        = string
+  description = "Name of the Talos cluster"
+  type        = string
 }
 
 variable "proxmox_control_pool_id" {
-    description = "Proxmox control VM pool ID"
-    type = string
-    default = null
+  description = "Proxmox control VM pool ID"
+  type        = string
+  default     = null
 }
 
 variable "proxmox_worker_pool_id" {
-    description = "Proxmox worker VM pool ID"
-    type = string
-    default = null
+  description = "Proxmox worker VM pool ID"
+  type        = string
+  default     = null
 }
 
 variable "talos_schematic_id" {
-    # Generate your own at https://factory.talos.dev/
-    # The this id has these extensions:
-    # qemu-guest-agent (required)
-    # If you make your own make sure you check this extension
-    # The ID is independent of the version and architecture of the image
-    description = "Schematic ID for the Talos cluster"
-    type        = string
-    default     = "ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515"
+  # Generate your own at https://factory.talos.dev/
+  # The this id has these extensions:
+  # qemu-guest-agent (required)
+  # If you make your own make sure you check this extension
+  # The ID is independent of the version and architecture of the image
+  description = "Schematic ID for the Talos cluster"
+  type        = string
+  default     = "ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515"
 }
 
 variable "talos_version" {
-    description = "Version of Talos to use"
-    type        = string
+  description = "Version of Talos to use"
+  type        = string
 }
 
 variable "talos_arch" {
-    description = "Architecture of Talos to use"
-    type        = string
-    default     = "amd64"
+  description = "Architecture of Talos to use"
+  type        = string
+  default     = "amd64"
 }
 
 # Theses two variables are maps that control how many control and worker nodes are created
@@ -129,49 +150,50 @@ variable "talos_arch" {
 #   "talos-worker-0" = "proxmox-node-0"
 #   "talos-worker-1" = "proxmox-node-0"
 # }
+
 variable "control_nodes" {
-    description = "Map of talos control node names to proxmox node names"
-    type        = map(string)
+  description = "Map of talos control node names to proxmox node names"
+  type        = map(string)
 }
 
 variable "worker_nodes" {
-    description = "Map of talos worker node names to proxmox node names"
-    type        = map(string)
+  description = "Map of talos worker node names to proxmox node names"
+  type        = map(string)
 }
 
 variable "control_machine_config_patches" {
-    description = "List of YAML patches to apply to the control machine configuration"
-    type        = list(string)
-    default     = [
-<<EOT
+  description = "List of YAML patches to apply to the control machine configuration"
+  type        = list(string)
+  default = [
+    <<EOT
 machine:
   install:
     disk: "/dev/vda"
 EOT
-    ]
+  ]
 }
 
 variable "worker_machine_config_patches" {
-    description = "List of YAML patches to apply to the worker machine configuration"
-    type        = list(string)
-    default     = [
-<<EOT
+  description = "List of YAML patches to apply to the worker machine configuration"
+  type        = list(string)
+  default = [
+    <<EOT
 machine:
   install:
     disk: "/dev/vda"
 EOT
-    ]
+  ]
 }
 
 variable "worker_extra_disks" {
-    # This allows for extra disks to be added to the worker VMs
-    # TODO - Should we allow other things like host PCI devices as well E.g., GPUs?
-    description = "Map of talos worker node name to a list of extra disk blocks for the VMs"
-    type        = map(list(object({
-        datastore_id = string
-        size         = number
-        file_format  = optional(string)
-        file_id      = optional(string)
-    })))
-    default     = {}
+  # This allows for extra disks to be added to the worker VMs
+  # TODO - Should we allow other things like host PCI devices as well E.g., GPUs?
+  description = "Map of talos worker node name to a list of extra disk blocks for the VMs"
+  type = map(list(object({
+    datastore_id = string
+    size         = number
+    file_format  = optional(string)
+    file_id      = optional(string)
+  })))
+  default = {}
 }
